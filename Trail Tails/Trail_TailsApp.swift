bf829db9a5e1e0dab50a5bfa9a995e46a0ecb7cf
Martin Flischman <martincodes@icloud.com>
2024-09-20 11:00:21 +0200

    Initial Commit

---
+++ Trail Tails/Trail_TailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trail_TailsApp.swift
+//  Trail Tails
+//
+//  Created by Martin on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Trail_TailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
