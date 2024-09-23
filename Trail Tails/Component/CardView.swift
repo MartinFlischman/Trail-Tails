4deb18dc24cf200fae50308d7d17070eb7ac2c76
Martin Flischman <martincodes@icloud.com>
2024-09-23 19:09:29 +0200

    Add settings sheet and restructure views in CardView
    feat: add settings sheet in CardView with isShowingSheet state
    
    feat: create SettingsView for the sheet content
    
    chore: move content view to new Screen folder
    
    feat: update button action in CardView to toggle sheet display

--- Trail Tails/Component/CardView.swift
+++ Trail Tails/Component/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // MARK: - Functions
     
@@ -51,8 +52,12 @@ struct CardView: View {
                         
                         Button {
                             // Action: Show a sheet
+                            isShowingSheet.toggle()
                         } label: {
                             CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingSheet) {
+                            SettingsView()
                         }
                     }
                     Text("A fun and enjoyable outdoor activity for friends and families")
---
+++ Trail Tails/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Trail Tails
+//
+//  Created by Martin on 2024/09/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
