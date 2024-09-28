9925a3b93fd11f5e81aa77159ad2569751a07a92
Martin Flischman <martincodes@icloud.com>
2024-09-28 14:02:09 +0200

    Add Alternate App Icons and Horizontal Scrollable Selection to SettingsView
    feat: add alternateAppIcons array to store available alternate app icons Stores the list of alternate icons for user selection
    
    feat: implement horizontal scroll view in SettingsView for selecting alternate app icons Allows users to scroll through and choose from multiple app icons
    
    feat: add functionality to update the app's icon when an alternate icon is selected Enables changing the app icon when a user taps on an alternate icon
    
    feat: display a preview image for each alternate icon in the scrollable list Shows a preview of each app icon for easier selection
    
    feat: add descriptive text encouraging users to select their favourite app icon Provides users with instructions to choose their preferred app icon

--- Trail Tails/Screen/SettingsView.swift
+++ Trail Tails/Screen/SettingsView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - Properties
+    
+    private let alternateAppIcons: [String] = [
+        "AppIcon-Mushroom",
+        "AppIcon-Map",
+        "AppIcon-Camera",
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Backpack"
+    ]
+    
+    
     var body: some View {
         List {
             // MARK: - Section: Header
@@ -63,6 +74,39 @@ struct SettingsView: View {
             
             // MARK: - Section: Icons
             
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button {
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                                    if error != nil {
+                                        print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                    } else {
+                                        print("You have updated the app's icon to \(alternateAppIcons[item]).")
+                                    }
+                                }
+                            } label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                            .buttonStyle(.borderless)
+                        }
+                    }
+                }
+                .padding(.top, 12)
+                
+                Text("Choose and set your favourite app icon.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
+            }
+            .listRowSeparator(.hidden)
             
             // MARK: - Section: About
             
